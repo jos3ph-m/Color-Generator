d471c4b0631266da829d710593ba46a446cc989a
Joseph M <jmedina1985@hotmail.com>
2021-03-18 22:23:48 -0700

    default list

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -6,6 +6,12 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const bcg = rgb.join(',');
   // const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}`;
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
 
   return (
     <article
