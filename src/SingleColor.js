3267fb02b0a1899a8107b3d1fd903ec0fcf1f8ab
Joseph M <jmedina1985@hotmail.com>
2021-03-18 19:53:06 -0700

    Single color set up

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -3,6 +3,8 @@ import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index }) => {
   const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+
   return <h4>single color</h4>;
 };
 
