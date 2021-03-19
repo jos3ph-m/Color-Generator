e5129a2c495451f2a998947bb063dbb0fd0d8e71
Joseph M <jmedina1985@hotmail.com>
2021-03-18 20:07:38 -0700

    Single color set up

--- src/App.js
+++ src/App.js
@@ -38,7 +38,14 @@ function App() {
       </section>
       <section className="colors">
         {list.map((color, index) => {
-          return <SingleColor key={index} {...color} index={index} />;
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
         })}
       </section>
     </>
